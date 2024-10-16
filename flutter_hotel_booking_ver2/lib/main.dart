6e70c03345d92ca222b225b9dbf9508ee725f372
QuocTan <163155776+VoQuocTanne@users.noreply.github.com>
2024-10-16 15:29:38 +0700

    firebase

--- flutter_hotel_booking_ver2/lib/main.dart
+++ flutter_hotel_booking_ver2/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_hotel_booking_ver2/firebase_options.dart';
 import 'package:flutter_hotel_booking_ver2/language/app_localizations.dart';
 import 'package:flutter_hotel_booking_ver2/logic/controllers/google_map_pin_controller.dart';
 import 'package:flutter_hotel_booking_ver2/logic/controllers/theme_provider.dart';
@@ -8,6 +10,9 @@ import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Get.putAsync<Loc>(() => Loc().init(), permanent: true);
 
